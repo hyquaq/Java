7f5b8265e340c4af08414344c4e3a0fa6e4043c2
hyquaq <hyquaq@gmail.com>
2021-04-05 16:24:11 +0700

    update code

--- Arithmetic Mean/index.java
+++ Arithmetic Mean/index.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+import java.util.*;
 // package Arithmetic Mean;
 
 public class index {
@@ -13,10 +15,20 @@ public class index {
     public static void main(String[] args) {
         int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9 };
 
+        // default
         double sum = sumArray(a);
         System.out.println(sum / a.length);
+
+        // printf from c
         System.out.printf("%.5f\n", sum / a.length);
+
+        // use String.format()
         String result = String.format("%.5f", sum / a.length);
         System.out.println(result);
+
+        // use DecimalFormat
+        DecimalFormat df = new DecimalFormat("#.####");
+        double ans = sum / a.length;
+        System.out.println(df.format(ans));
     }
 }
