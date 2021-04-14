b3dbf27045628d99255ec913c6236c30be7a45fd
hyquaq <hyquaq@gmail.com>
2021-04-14 19:32:20 +0700

    add Shape Circle Rectangle Square

---
+++ Lab[number]/Lab6/4/Shape.java
@@ -0,0 +1,35 @@
+public class Shape {
+    private String color;
+    private boolean filled;
+
+    public Shape() {
+        this.color = "red";
+        this.filled = true;
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return this.filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape[Color = " + this.color + ", Filled = " + this.filled + "]";
+    }
+}
