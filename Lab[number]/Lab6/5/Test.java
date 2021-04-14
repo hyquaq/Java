f2f58693015d20a78995ba192c9cd1c3bf902e35
hyquaq <hyquaq@gmail.com>
2021-04-14 19:55:27 +0700

    add Employee

---
+++ Lab[number]/Lab6/5/Test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Employee employee = new Employee("52000668", "NguyenHuy", 2019, 1.0, 4);
+        System.out.println(employee);
+        Employee employee1 = new Employee("52000668", "NguyenHuy", 2020, 1.0, 2);
+        System.out.println(employee1);
+    }
+}
