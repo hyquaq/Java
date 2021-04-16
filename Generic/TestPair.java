a09e96798baff5a09cc1a6e22a9942fe751b86f0
hyquaq <hyquaq@gmail.com>
2021-04-16 22:11:22 +0700

    how to creat method use Generic

--- Generic/TestPair.java
+++ Generic/TestPair.java
@@ -1,4 +1,10 @@
 public class TestPair {
+    public static <T> void printArray(T[] a) {
+        for (T value : a) {
+            System.out.println(value);
+        }
+    }
+
     public static void main(String[] args) {
         Pair<Double> point = new Pair(1, 2);
         System.out.println(point);
@@ -7,5 +13,13 @@ public class TestPair {
         System.out.println("name: " + person.getFirst());
         System.out.println("age: " + person.getSecond());
         System.out.println(person);
+
+        // test method Generic
+        Integer[] a = { 1, 2, 3, 4, 5 };
+        Double[] b = { 1.1, 1.2, 1.3 };
+        String[] name = { "a", "b", "c", "d", "e" };
+        printArray(a);
+        printArray(b);
+        printArray(name);
     }
 }
