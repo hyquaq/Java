5cead57de71f6689b8d1974022b96ea64dd2707e
Huy Nguyen <hyquaq@gmail.com>
2021-10-24 17:29:29 +0700

    khung ex1 lab5

---
+++ algorithm and structure/Lab5/1/IntLinkedList.java
@@ -0,0 +1,30 @@
+class IntLinkedList {
+
+    private Node head;
+
+    IntLinkedList() {
+    }
+
+    public void addFirst(int data) {
+
+    }
+
+    public boolean addLast(int data) {
+        return false;
+    }
+
+    public boolean removeAt(int position) {
+        return false;
+    }
+
+    public int countOdd() {
+    }
+
+    public int searchKey(int key) {
+
+    }
+
+    public boolean checkSorted() {
+        return false;
+    }
+}
---
+++ algorithm and structure/Lab5/1/Node.java
@@ -0,0 +1,29 @@
+class Node {
+    private int data = 0;
+    private Node next = null;
+
+    Node() {
+
+    }
+
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
