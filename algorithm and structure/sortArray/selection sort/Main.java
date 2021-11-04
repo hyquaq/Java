451ce06224619c2deca27f0fd20e844f67279c3b
Huy Nguyen <hyquaq@gmail.com>
2021-11-04 22:58:56 +0700

    update

--- algorithm and structure/sortArray/selection sort/Main.java
+++ algorithm and structure/sortArray/selection sort/Main.java
@@ -28,30 +28,39 @@ public class Main {
         }
     }
 
+
+    public static void sortV2(int[] a) {
+        for (int i = a.length-1; i >= 0; i--) {
+            int max = i;
+
+            // find max
+            for (int j = 0; j < i; j++) {
+                if (a[max] < a[j]) {
+                    max = j;
+                }
+            }
+
+            if (i == max) {
+                continue;
+            }
+            // swap
+            // System.out.println(a[max]);
+            int temp = a[max];
+            a[max] = a[i];
+            a[i] = temp;
+            // System.out.println(Arrays.toString(a));
+
+        }
+    }
+
     public static void main(String[] args) {
 
         // int[] a = { 2, 1, 3, 0, 5 };
         int[] a = { 2, 1, 3, 0, 5, 10, -10, 30, -29, -27, 5, 3, -3, -2 };
 
-        sort(a);
+        System.out.println(Arrays.toString(a)); 
+        sortV2(a);
         System.out.println(Arrays.toString(a));
-
-        // for (int i = 0; i < a.length - 1; i++) {
-
-        // int min_index = i;
-        // for (int j = i + 1; j < a.length; j++) {
-        // if (a[min_index] > a[j]) {
-        // min_index = j;
-        // }
-        // }
-
-        // // swap
-        // int temp = a[i];
-        // a[i] = a[min_index];
-        // a[min_index] = temp;
-        // }
-
-        // System.out.println(Arrays.toString(a));
 
     }
 }
